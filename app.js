dce8d68743ab05f9e1c61c890130116807aee5c7
Dan <dan@dan.com>
2016-10-23 14:50:59 -0400

    Fix routing

--- app.js
+++ app.js
@@ -25,9 +25,9 @@ app.use(cookieParser());
 app.use('/s/static/',express.static(path.join(__dirname, 'public')));
 app.use('/static/',express.static(path.join(__dirname, 'public')));
 
-app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
-app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/s/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/s/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/s/js', express.static(__dirname + '/node_modules/jquery/dist'));
 
 app.use('/s/:id', streamIndex);
 app.use('/s', streamIndex);

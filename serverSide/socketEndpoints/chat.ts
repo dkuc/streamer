b07636bf0c9022b518bb87fc4fefab3fea996715
Dan <dan@dan.com>
2016-10-23 17:57:17 -0400

    Block duplicate names

--- serverSide/socketEndpoints/chat.ts
+++ serverSide/socketEndpoints/chat.ts
@@ -2,37 +2,18 @@ import ws = require('./websocket');
 import _ = require('lodash');
 
 const server = ws.getServer();
-const storedNames = {'::1': 'Rack'};
+const storedNames = {'192.168.1.65': 'Dan'};
 const sockets = [];
 
-//ToDo: Stop duplicate names, name length limits
-server.on('connection', function(userSocket){
-    userSocket.on('chat message', function(msg){
-        server.emit("chat", {message:msg, sender:userSocket.chatName});
-    });
-
-    userSocket.on('disconnect', function(){
-       console.log('removing: ' +userSocket.realConnection);
-        _.remove(sockets, userSocket);
-        emitUsers();
-    });
+server.on('connection', function (userSocket) {
+    userSocket.on('chat message', onChatMessage);
+    userSocket.on('disconnect', onDisconnect);
+    userSocket.on('setname', onSetName);
 
 
-
-    userSocket.on('setname', function(name){
-        userSocket.chatName = name;
-
-        storedNames[userSocket.realConnection] = name;
-
-        userSocket.emit('name', name);
-        emitUsers();
-
-    });
-
-
-    userSocket.chatName = userSocket.id.substr(2,5);
+    userSocket.chatName = userSocket.id.substr(2, 5);
     var storedName = storedNames[userSocket.realConnection];
-    if(storedName) {
+    if (storedName) {
         userSocket.chatName = storedName;
     }
     storedNames[userSocket.realConnection] = userSocket.chatName;
@@ -44,23 +25,50 @@ server.on('connection', function(userSocket){
     emitUsers();
 
 
-    function emitUsers() {
-
-        const usersWithIps  =_.map(sockets, function (socket) {
-            return `${socket.chatName} (${socket.realConnection})`;
-        });
-
-        const users  =_.map(sockets, 'chatName');
-
-        for (var socket of sockets) {
-            if (isLocal(socket))
-                socket.emit('users', usersWithIps);
-            else
-                socket.emit('users', users);
-        }
-
+    function onChatMessage(msg) {
+        server.emit("chat", {message: msg, sender: userSocket.chatName});
     }
+
+    function onDisconnect() {
+        console.log('removing: ' + userSocket.realConnection);
+        _.remove(sockets, userSocket);
+        emitUsers();
+    }
+
+    function onSetName(name) {
+        if (name.length > 10)
+            return;
+
+        if (_.some(sockets, (socket) => socket.chatName === name))
+            return;
+
+        userSocket.chatName = name;
+
+        storedNames[userSocket.realConnection] = name;
+
+        userSocket.emit('name', name);
+        emitUsers();
+    }
+
 });
+
+
+function emitUsers() {
+
+    const usersWithIps = _.map(sockets, function (socket) {
+        return `${socket.chatName} (${socket.realConnection})`;
+    });
+
+    const users = _.map(sockets, 'chatName');
+
+    for (var socket of sockets) {
+        if (isLocal(socket))
+            socket.emit('users', usersWithIps);
+        else
+            socket.emit('users', users);
+    }
+
+}
 
 function isLocal(socket) {
     return socket.realConnection.includes('192.168.') ||
